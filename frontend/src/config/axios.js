70f8801f453bba2e6ea071d198d9a55de670df87
imtiazsolangi@gmail.com <imtiazsolangi@gmail.com>
2024-08-05 11:21:39 +0500

    serverurl deployment changes

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import { default as axios_lib } from "axios";
 
 const axios = axios_lib.create({
-  baseURL: "http://localhost:2100/api",
+  baseURL: "https://chat-app-qxrf.onrender.com/api",
 });
 
 export default axios;
